c88e9de4ae720a4613027896e3374acd31d91520
DESKTOP-SS7H09L\drell <p.hundelshausen@moodia.net>
2019-12-08 20:41:06 +0100

    build dat plugin

--- killDevtools.js
+++ killDevtools.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
 try {
-  fs.unlinkSync('C:\\Users\\Philipp\\AppData\\Roaming\\Hyper\\DevTools Extensions');
+  fs.unlinkSync('C:\\Users\\drell\\AppData\\Roaming\\Hyper\\DevTools Extensions');
 } catch(e) {}
